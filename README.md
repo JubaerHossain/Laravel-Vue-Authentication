866a61052f63d6f94e15036ae0ef0b9c0472ac7e
JubaerHossain <jubaer0001@gmail.com>
2018-11-25 20:39:45 +0600

    creating simple api and login system using vue

---
+++ README.md
@@ -0,0 +1,10 @@
+
+* Run ` composer install `
+* Save as the `.env.example` to `.env` and set your database information 
+* Run ` php artisan key:generate` to generate the app key
+* Run ` npm install ` 
+* Run ` php artisan migrate ` 
+
+
+
+# Laravel+Vue Authentication
---
+++ resources/assets/js/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h3> Not Found What you're looking </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        }
+    }
+</script>
